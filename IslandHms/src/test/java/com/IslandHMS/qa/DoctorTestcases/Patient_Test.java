4b9e0e2579bb2f7489a88c508252f0f3fdb6d447
Dharsaikat <dharsaikat8@gmail.com>
2018-03-10 21:07:34 +0530

    second change

---
+++ IslandHms/src/test/java/com/IslandHMS/qa/DoctorTestcases/Patient_Test.java
@@ -0,0 +1,61 @@
+package com.IslandHMS.qa.DoctorTestcases;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.IslandHMS.qa.DoctorPages.Patients;
+import com.IslandHMS.qa.LoginAll.LoginAll;
+import com.IslandHMS.qa.base.TestBase;
+
+public class Patient_Test extends TestBase {
+	
+	LoginAll loginAll;
+	Patients patient;
+	
+	public Patient_Test()
+	{
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginAll = new LoginAll();
+		patient = new Patients();
+	}
+	
+	@Test(priority=1)
+	public void doctorLogin_Test()
+	{
+		loginAll.doctorLogin(prop.getProperty("doctor_username"),prop.getProperty("doctor_password"));
+		
+	}
+	
+	
+	@Test(priority=2)
+	public void new_patient_add_button_click_Test()
+	{
+		patient.new_patient_add_button_click();
+	}
+	
+	@Test(priority=3)
+	public void new_patients_add_url_check_Test()
+	{
+		patient.new_patients_add_url_check();
+	}
+    @Test(priority=4)
+    public void new_patient_form_fillup_Test()
+    {
+    	patient.new_patient_form_fillup("papai", "dhar", "kumar", "male", "5651547", "23/05/1991");
+    }
+    @Test(priority=5)
+    public void new_patient_add_Test()
+    {
+    	patient.new_patient_add();
+    }
+    @Test(priority=6)
+    public void search_patient_in_patient_listing_Test()
+    {
+    	patient.search_patient_in_patient_listing("papai");
+    }
+}
